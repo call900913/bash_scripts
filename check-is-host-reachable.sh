676ad9a7d36853c18379865c43cb1a8852c0df29
Ryan Christy <call900913@icloud.com>
2022-03-23 07:19:10 -0400

    Refactor most recently added script

--- check-is-host-reachable.sh
+++ check-is-host-reachable.sh
@@ -3,11 +3,11 @@
 echo "Enter the hostname in this format: example.com, github.com, etc"
 read -p "Enter the hostname you want to ping: " HOST
 
-ping -c 1 $HOST
+ping -c 1 $HOST && echo "$HOST is reachable" || echo "$HOST is not reachable."
 
-if [ "$?" -eq "0" ]
-then
-  echo "$HOST is reachable."
-else
-  echo "$HOST is not reachable."
-fi
+# if [ "$?" -eq "0" ]
+# then
+#  echo "$HOST is reachable."
+# else
+#  echo "$HOST is not reachable."
+# fi
