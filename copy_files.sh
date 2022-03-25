e2cf98f6f3d9ccc69cb6ebe24dd492373658221b
Ryan Christy <call900913@icloud.com>
2022-03-25 08:45:16 -0400

    Complete the copy_files script

--- copy_files.sh
+++ copy_files.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-# This script makes copies of files and places them in the directory you specify.
-# use it like so: ./copy_ciles.sh 
+# This script makes a copy of the file you enter in the prompt
+# and places it in a folder named copies/ inside the current directory
+read -p "Enter name of the file: " FILE
 
+function copy_files() {
+  mkdir copies/
+  if [ -f $1 ]
+  then
+    local COPY="./copies/$(basename FILE).$(date +%F).$$"
+    echo "Copying $FILE"
+    cp $FILE copies/
+  else
+    # exit the function with a non-zero exit status
+    return 1
+  fi
+}
 
+copy_files
+
+if [ $? -eq 0 ]
+then 
+  echo "Copy successful."
+else
+  echo "Copy failed."
+  exit 1
+fi
