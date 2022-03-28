18abf902c9c8ae97b7e7f4ab277c1eb88c539433
Ryan Christy <call900913@icloud.com>
2022-03-28 05:46:49 -0400

    Update the while script

--- while.sh
+++ while.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 # a block of code that tests a given condition.
-# In this case, that condition is the name you entered and your confirmation.
+# In this case, that condition is the hostname you entered and your confirmation.
 
+echo "Script will run until the app is no longer reachable. To terminate this script, press ctrl-C."
+echo "Enter the hostname in this format: example.com, github.com, etc"
+read -p "Enter the name of the host you want to ping: " HOST
 
-while [ "$ANSWER" != "y" ]
+# while [ "$ANSWER" != "y" ]
+# do
+#   read -p "Enter your name: " NAME
+#   read -p "${NAME}; do I have that right? (y/n)" ANSWER
+# done
+
+while ping -c 1 $HOST >> /dev/null
 do
-  read -p "Enter your name: " NAME
-  read -p "${NAME}; do I have that right? (y/n)" ANSWER
+  echo "App still up"
+  sleep 2
 done
